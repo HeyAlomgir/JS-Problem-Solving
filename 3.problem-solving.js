1837345850bba5b557dec94cf0ae9118424ddf78
HeyAlomgir <alomgirhosssain71@gmail.com>
2026-02-05 23:25:23 +0600

    4.problem-solving-basic.js

--- 3.problem-solving.js
+++ 3.problem-solving.js
@@ -5,13 +5,13 @@ Rules:
 Length must be at least 8
 
 
-Must contain at least 1 number
+1.Must contain at least 1 number
 
 
-Must contain at least 1 uppercase letter
+2.Must contain at least 1 uppercase letter
 
 
-Must not contain spaces
+3.Must not contain spaces
 
 
 Test case 1
@@ -19,116 +19,70 @@ Input:  "helloWorld"
 Output:
 { valid: false, reasons: ["missing number"] }
 
-Test case 2
-Input:  "Hello123"
-Output: { valid: true, reasons: [] }
+
 */ 
 
 
 
-// function checkPassword(password){
-//     let length = password.length;
-//     let hasUpperCase =false;
-//     let hasNumber = false;
-//     // console.log(password,length);
-
-//     let reasons = [];
-//     let hasSpace = password.includes(" ")
-
-    
-//     for( let i=0; i<length; i++){
-//         let character = password[i];
-//         console.log(character)
-        
-//         if(character >= "0" && character <= "9"){
-//             hasNumber = true;
-//         }
-//         if(character >= "A" && character <= "Z"){
-//             hasUpperCase = true;
-//         }
-//     }
-//     console.log( password,hasNumber,hasUpperCase);
-//     if(!hasNumber){
-//         reasons.push("Missing number");
-//     }
-//     if(!hasUpperCase){
-//         reasons.push("Missing uppercase");
-//     }
-
-//     if(hasSpace == true){
-//         reasons.push("Space found!!!!!");
-//     }
-
-//     //  console.log(hasSpace);
-
-//     let invalid = reasons.length == 0;
-//     console.log(invalid);
-   
-//     return{
-//         valid:invalid,
-//         reasons,
-//     }
-
-    
-    
-// }
-
-// let Output = checkPassword( "helloWorld");
-// console.log(Output);
-
-
-
-
-
-
-
-// { valid: false, reasons: ["missing number"] }
-
-
-function checkPassword (password){
+function checkPassword(password){
     let length = password.length;
-    let hasNumber=false;
-    let hasUpperCase=false;
-    let reasons=[];
-    let hasSpace = password.includes(" ");
-  
-    for(let i = 0 ; i<length; i++){
-        let character = password[i];
-        // console.log(character);
+    let hasUpperCase =false;
+    let hasNumber = false;
+    // console.log(password,length);
 
-        if(character >="0" && character <= "9"){
-            hasNumber=true;
+    let reasons = [];
+    let hasSpace = password.includes(" ")
+
+    
+    for( let i=0; i<length; i++){
+        let character = password[i];
+        console.log(character)
+        
+        if(character >= "0" && character <= "9"){
+            hasNumber = true;
         }
-        if(character >="A" && character <="Z"){
-            hasUpperCase=true;
+        if(character >= "A" && character <= "Z"){
+            hasUpperCase = true;
         }
     }
-    // console.log(password, length);
+    console.log( password,hasNumber,hasUpperCase);
     if(!hasNumber){
         reasons.push("Missing number");
     }
     if(!hasUpperCase){
-        reasons.push("Mising uppercase");
+        reasons.push("Missing uppercase");
     }
 
-    // console.log(hasSpace);
-
-    if( hasSpace == true){
-        reasons.push("Space found !!!!")
+    if(hasSpace == true){
+        reasons.push("Space found!!!!!");
     }
+
+    //  console.log(hasSpace);
 
     let invalid = reasons.length == 0;
-    // console.log(invalid);
-
+    console.log(invalid);
+   
     return{
         valid:invalid,
         reasons,
     }
 
-
+    
+    
 }
-let Output = checkPassword("helloWorld");
+
+let Output = checkPassword( "helloWorld");
 console.log(Output);
+
+
+
+
+
+
+
+
+
+
 
 
 
